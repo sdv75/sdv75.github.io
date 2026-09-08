91f3b749625e4563b13d8de87b2cb4c18f90811d
sdv75 <sdv75@github.com>
2026-09-08 21:27:36 +0300

    fix: basePath for lp_glamping assets 404

--- lp/lp_glamping/_next/static/UaRhfExLYfdse_jsTEDqd/_buildManifest.js
+++ lp/lp_glamping/_next/static/UaRhfExLYfdse_jsTEDqd/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/lp/lp_glamping//_next/:path+",
+        "destination": "/lp/lp_glamping/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
